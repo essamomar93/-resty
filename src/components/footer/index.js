6ced000e895edcfccbc1105b1c29490f885ac3b2
essam omar <essam.93@hotmail.com>
2021-12-05 17:35:04 +0200

    lab 26

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,12 +1,3 @@
-// import React from 'react';
-
-// class Footer extends React.Component {
-//   render() {
-//     return <footer>&copy; 2018</footer>;
-//   }
-// }
-
-// export default Footer;
 
 export default function Footer(){
   return <footer>&copy; 2018</footer>;
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,16 +1,3 @@
-// import React from 'react';
-
-// class Header extends React.Component {
-//   render() {
-//     return (
-//       <header>
-//         <h1>RESTy</h1>
-//       </header>
-//     );
-//   }
-// }
-
-// export default Header;
 
 export default function Header() {
   return (
