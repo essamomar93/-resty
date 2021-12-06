6ff483f4b88c94050962fdf7388d74ef6f6df00b
essam omar <essam.93@hotmail.com>
2021-12-06 20:57:37 +0200

    lab27

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,30 +1,62 @@
+import React, { useState } from "react";
 
-import React from 'react';
-import './form.scss';
+import "./form.scss";
 export default function Form(myProps) {
+  const [methods, setMethods] = useState(0);
+  const [Url, setUrl] = useState(0);
+  const [showTextArea, setShowArea] = useState(false);
+  let [requestBody, setrequestBody] = useState("");
 
-  const handleSubmit = e => {
+  const handelPost = (e) => {
     e.preventDefault();
-    const formData = {
-      method: 'GET',
-      url: 'https://pokeapi.co/api/v2/pokemon',
+    setShowArea(!showTextArea);
+    setMethods(e.target.value);
+  };
+  const getPost = (e) => {
+    e.preventDefault();
+    setrequestBody(e.target.value);
+  };
+  const handleMethod = (e) => {
+    e.preventDefault();
+    setMethods(e.target.value);
+  };
+  const handleUrl = (e) => {
+    e.preventDefault();
+    setUrl(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    requestBody;
+    const getData = {
+      method: methods,
+      url: Url
     };
-    myProps.handleApiCall(formData);
-  }
+
+    myProps.handleApiCall(getData);
+  };
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <label >
+        <div className="go">
           <span>URL: </span>
-          <input name='url' type='text' />
+          <input name="url" type="text" onChange={handleUrl} />
           <button type="submit">GO!</button>
-        </label>
-        <label className="methods">
-          <span id="get">GET</span>
-          <span id="post">POST</span>
-          <span id="put">PUT</span>
-          <span id="delete">DELETE</span>
-        </label>
+        </div>
+        <div className="methods">
+          <button value="GET" onClick={handleMethod}>
+            GET
+          </button>
+          <button value="POST" onClick={handelPost}>
+            POST
+          </button>
+          <button value="PUT" onClick={handelPost}>
+            PUT
+          </button>
+          <button value="DELETE" onClick={handleMethod}>
+            DELETE
+          </button>
+        </div>
+        <input className="text" name="url" type="text" onChange={getPost} />
       </form>
     </>
   );
